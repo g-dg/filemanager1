74f09f8fa583e71ba316b854427674a7ec20093d
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-08-02 19:37:48 -0400

    Set time limit when not serving files to 10 mins

--- init.php
+++ init.php
@@ -1,3 +1,8 @@
 <?php
 require_once('version.php');
+
 $GLOBALS['script_start_time'] = microtime(true);
+
+// run for 10 minutes max
+// (when serving files, this is overwritten)
+set_time_limit(600);
