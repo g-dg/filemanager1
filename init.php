5c06f4b152f5852f023b4da6ac715d63657cfb60
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-08-18 19:04:55 -0400

    Minor comment fix

--- init.php
+++ init.php
@@ -3,6 +3,6 @@ require_once('version.php');
 
 $GLOBALS['script_start_time'] = microtime(true);
 
-// run for 10 minutes max
+// run for at least 10 minutes
 // (when serving files, this is overwritten)
 set_time_limit(600);
