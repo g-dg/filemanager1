8958356126fc4294ab3d8a3dbe490630ebdc3d63
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-08-18 18:42:04 -0400

    Fix error displayed on logout and add logout message

--- logout.php
+++ logout.php
@@ -5,5 +5,6 @@ session_start();
 session_unset();
 //session_regenerate_id(true);
 //setcookie(session_name(), '', time() - 1);
-header('Location: '.pathinfo($_SERVER['SCRIPT_NAME'])['dirname'].'/');
+$_SESSION['msg'] = 'User logged out.';
+header('Location: '.pathinfo($_SERVER['SCRIPT_NAME'])['dirname'].'/login.php');
 exit();
