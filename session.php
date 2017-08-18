c6a3151a25f3f7206ab881f80dcf475ab695f30c
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-08-18 19:47:49 -0400

    Don't log out other user in session hijack

--- session.php
+++ session.php
@@ -19,19 +19,15 @@ function startSession()
 }
 
 // check the IP address, used for detecting shared links and forged sessions
-//TODO: Write my own session handler that works better for this (doesn't have to log out the other user)
 function checkUserIP()
 {
 	if (isset($_SESSION['user_ip']))
 	{
 		if ($_SERVER['REMOTE_ADDR'] !== $_SESSION['user_ip'])
 		{
-			session_unset();
-			session_regenerate_id(true);
-			session_write_close();
-			setcookie(session_name(), '', time() - 1);
+			setcookie(session_name(), '', time() - 3600);
 			http_response_code(401);
-			die();
+			die('Possible session hijack attempt detected!');
 		}
 	}
 	else
