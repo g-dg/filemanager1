a3865b603a175ae987a45f5ad185b24c8bb677de
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-09-02 15:02:36 -0400

    Use prettified last modified

--- listing.php
+++ listing.php
@@ -274,7 +274,7 @@ function serveDirectoryListing($share, $path)
 						echo '<tr>'.
 								'<td class="icon"><span title="File"><img src="'.htmlentities(pathinfo($_SERVER['SCRIPT_NAME'])['dirname']).'/icon/generic.gif" alt="[FILE]" width="20" height="22"></span></td>'.
 								'<td><span title="'.htmlentities($file['name']).'"><a href="'.htmlentities($file['uri']).'?'.urlencode(session_name()).'='.urlencode(session_id()).'" target="_blank">'.htmlentities($file['name']).'</a></span></td>'.
-								'<td><span title="'.htmlentities(prettifyDate($file['last_modified'])).'">'.htmlentities(date(GD_FILEMANAGER_DATE_FORMAT, $file['last_modified'])).'</span></td>'.
+								'<td><span title="'.htmlentities(date(GD_FILEMANAGER_DATE_FORMAT, $file['last_modified'])).'">'.htmlentities(prettifyDate($file['last_modified'])).'</span></td>'.
 								'<td><span title="'.htmlentities(number_format($file['size'], 0, '.', ',')).' bytes">'.htmlentities(prettifyFileSize($file['size'])).'</span></td>'.
 								'<td><span title="Download '.htmlentities($file['name']).'"><a href="'.htmlentities($file['uri']).'?'.urlencode(session_name()).'='.urlencode(session_id()).'&amp;download">Download</a></span></td>'.
 								'</tr>';
@@ -285,7 +285,7 @@ function serveDirectoryListing($share, $path)
 						echo '<tr>'.
 								'<td class="icon"><span title="Directory"><img src="'.htmlentities(pathinfo($_SERVER['SCRIPT_NAME'])['dirname']).'/icon/folder.gif" alt="[DIR]" width="20" height="22"></span></td>'.
 								'<td><span title="'.htmlentities($file['name']).'/"><a href="'.htmlentities($file['uri']).'">'.htmlentities($file['name']).'/</a></span></td>'.
-								'<td><span title="'.htmlentities(prettifyDate($file['last_modified'])).'">'.htmlentities(date(GD_FILEMANAGER_DATE_FORMAT, $file['last_modified'])).'</span></td>'.
+								'<td><span title="'.htmlentities(date(GD_FILEMANAGER_DATE_FORMAT, $file['last_modified'])).'">'.htmlentities(prettifyDate($file['last_modified'])).'</span></td>'.
 								'<td><span title="Contains '.htmlentities(pluralize(number_format($file['size'], 0, '.', ','), 'file')).'">'.htmlentities(prettifyFileCount($file['size'])).'</span></td>'.
 								'<td><span title="Download not available"><em>N/A</em></span></td>'.
 								'</tr>';
